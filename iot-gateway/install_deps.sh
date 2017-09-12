b291bd296ef21be082600e476c8c15fdeaeef4c3
Kalonji Bankole <kkbankol@us.ibm.com>
2017-09-12 11:34:58 -0700

    adding submodule init/update for 433Utils

--- iot-gateway/install_deps.sh
+++ iot-gateway/install_deps.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 sudo apt-get -y update
-sudo apt-get -y install git-core
+sudo apt-get -y install git-core build-essential
+cd ~/
+rm -rf wiringPi
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 git pull origin master
 ./build
-cd ~
-sudo apt-get install build-essential
-git clone git://github.com/ninjablocks/433Utils.git
+cd ~/
+rm -rf 433Utils
+git clone --recursive git://github.com/ninjablocks/433Utils.git
 cd 433Utils/RPi_utils
 make
