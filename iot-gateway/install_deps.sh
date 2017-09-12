4ff19fb6380ad73a1c62fb677b7474ab5d03b4e9
Kalonji Bankole <kkbankol@us.ibm.com>
2017-09-12 11:27:40 -0700

    fixing path error for install_deps script

--- iot-gateway/install_deps.sh
+++ iot-gateway/install_deps.sh
@@ -2,8 +2,9 @@
 sudo apt-get -y update
 sudo apt-get -y install git-core
 git clone git://git.drogon.net/wiringPi
+cd wiringPi
 git pull origin master
-./wiringPi/build
+./build
 cd ~
 sudo apt-get install build-essential
 git clone git://github.com/ninjablocks/433Utils.git
