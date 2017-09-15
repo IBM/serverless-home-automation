38fda597ef29afa6e997a83c12b27f78c2620d1f
Kalonji Bankole <kkbankol@us.ibm.com>
2017-09-15 11:12:47 -0700

    adding dependencies for snowboy

--- iot-gateway/install_deps.sh
+++ iot-gateway/install_deps.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
+
 sudo apt-get -y update
-sudo apt-get -y install git-core build-essential
+sudo apt-get -y install git-core build-essential python-pyaudio python3-pyaudio sox make pulseaudio alsa-utils
+sudo easy_install pip
+
+## Swig
+sudo apt-get -y install libpcre3 libpcre3-dev autoconf
+#sudo apt-get -y install swig3.0 # apt-get version outdated, have to build from source
+#TODO, add validated swig url
+#wget http://ufpr.dl.sourceforge.net/project/swig/swig/swig-3.0.12/swig-3.0.12.tar.gz
+git clone https://github.com/swig/swig
+#tar -xzf swig-3.0.12.tar.gz
+#cd swig-3.0.12/
+#./configure
+#make
+#make install
+
+## Snowboy
+cd ~/
+sudo apt-get -y install libatlas-base-dev
+sudo pip install pyaudio
+git clone https://github.com/Kitt-AI/snowboy/
+cd snowboy/swig/Python
+make
+
+
+## wiringPi
 cd ~/
 rm -rf wiringPi
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 git pull origin master
 ./build
+
+## 433Utils
 cd ~/
 rm -rf 433Utils
 git clone --recursive git://github.com/ninjablocks/433Utils.git
---
+++ recording-device/README.md
@@ -0,0 +1,36 @@
+### Speech Recorder
+After plugging in the microphone, obtain the sound card number with the following command
+```
+pi@raspberrypi2:/tmp $ cat /proc/asound/cards 
+ 0 [ALSA           ]: bcm2835 - bcm2835 ALSA
+                      bcm2835 ALSA
+ 1 [Dummy          ]: Dummy - Dummy
+                      Dummy 1
+ 2 [Device         ]: USB-Audio - USB PnP Sound Device
+                      C-Media Electronics Inc. USB PnP Sound Device at usb-3f980000.usb-1.3, full spe
+```
+
+We see that our USB-Audio is card 2, so we'll need to reflect that in our ~/.asoundrc file
+```
+pi@raspberrypi2:/tmp $ cat ~/.asoundrc
+pcm.!default  {
+ type hw card 2
+}
+ctl.!default {
+ type hw card 2
+}
+```
+
+
+Troubleshooting
+```
+# jack server is not running or cannot be started
+
+DISPLAY=:0 jack_control start
+pulseaudio --start
+```
+
+Suppress false errors
+https://github.com/Kitt-AI/snowboy/issues/9
+
+
