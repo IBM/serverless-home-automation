4464564275a5f2a93894a2869810aa3bb8dc9329
Steve Martinelli <stevemart@users.noreply.github.com>
2018-03-22 09:50:08 -0400

    rename variable from dict to iot_obj
    dict will shadow a built-in python name

--- ibm_cloud_functions/iot-pub.py
+++ ibm_cloud_functions/iot-pub.py
@@ -1,11 +1,11 @@
 import requests
 
 
-def main(dict):
-    iot_org_id = dict['iot_org_id']
-    device_id = dict['device_id']
-    device_type = dict['device_type']
-    api_token = dict['api_token']
-    classes = [nlc_class for nlc_class in dict['msg']['classes'] if nlc_class['confidence'] > 0.1]
+def main(iot_obj):
+    iot_org_id = iot_obj['iot_org_id']
+    device_id = iot_obj['device_id']
+    device_type = iot_obj['device_type']
+    api_token = iot_obj['api_token']
+    classes = [nlc_class for nlc_class in iot_obj['msg']['classes'] if nlc_class['confidence'] > 0.1]
     requests.post('http://' + iot_org_id + '.messaging.internetofthings.ibmcloud.com:1883/api/v0002/device/types/' + device_type + '/devices/' + device_id + '/events/query', headers={'Content-Type': 'application/json'}, json=classes, auth=('use-token-auth', api_token))
-    return {'msg': dict['msg']['text']}
+    return {'msg': iot_obj['msg']['text']}
