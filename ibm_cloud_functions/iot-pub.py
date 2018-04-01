6cf4626d0a23e338ce7bccc0eab10cbeffd0be2b
Kalonji Bankole <kkbankol@us.ibm.com>
2018-03-31 19:22:14 -0700

    updating iot-pub action to expect conversation output

--- ibm_cloud_functions/iot-pub.py
+++ ibm_cloud_functions/iot-pub.py
@@ -1,11 +1,9 @@
 import requests
-
-
 def main(iot_obj):
     iot_org_id = iot_obj['iot_org_id']
     device_id = iot_obj['device_id']
     device_type = iot_obj['device_type']
     api_token = iot_obj['api_token']
-    classes = [nlc_class for nlc_class in iot_obj['msg']['classes'] if nlc_class['confidence'] > 0.1]
-    requests.post('http://' + iot_org_id + '.messaging.internetofthings.ibmcloud.com:1883/api/v0002/device/types/' + device_type + '/devices/' + device_id + '/events/query', headers={'Content-Type': 'application/json'}, json=classes, auth=('use-token-auth', api_token))
-    return {'msg': iot_obj['msg']['text']}
+    payload = {"d": iot_obj['msg']}
+    requests.post('http://' + iot_org_id + '.messaging.internetofthings.ibmcloud.com:1883/api/v0002/device/types/' + device_type + '/devices/' + device_id + '/events/query', headers={'Content-Type': 'application/json'}, json=payload, auth=('use-token-auth', api_token))
+    return {"msg": payload}
