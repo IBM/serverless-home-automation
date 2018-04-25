7e223d014e07d49bc6c2db66c4f810825e1a7636
Kalonji Bankole <kkbankol@us.ibm.com>
2018-04-25 14:30:45 -0400

    fixing travis error

--- ibm_cloud_functions/iot-pub.py
+++ ibm_cloud_functions/iot-pub.py
@@ -1,3 +1,4 @@
+# This function publishes the results from the Watson Assistant service to the Watson IoT platform
 import requests
 def main(iot_obj):
     # import IOT platform credentials
@@ -5,8 +6,8 @@ def main(iot_obj):
     device_id = iot_obj['device_id']
     device_type = iot_obj['device_type']
     api_token = iot_obj['api_token']
-    # extract result from configuration service
+    # extract result from Watson Assistant
     payload = {"d": iot_obj['msg']}
-    # publish conversation intent/entity pair to Watson IOT platform and subsequently all subscribed devices
+    # publish Watson Assistant intent/entity pair to Watson IOT platform and subsequently all subscribed devices
     requests.post('https://' + iot_org_id + '.messaging.internetofthings.ibmcloud.com:8883/api/v0002/device/types/' + device_type + '/devices/' + device_id + '/events/query', headers={'Content-Type': 'application/json'}, json=payload, auth=('use-token-auth', api_token))
     return {"msg": payload}
